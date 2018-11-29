504d0f279ba951fcd5387320a268837825a0896e
Juyoung Kim <JYK3216@gmail.com>
2018-11-29 14:46:47 -0500

    All category id and names done

--- uniquecat.py
+++ uniquecat.py
@@ -1,16 +1,28 @@
 import requests
 import csv
 import io
+import sys
 
+id = 0
 category = 4
 categories = []
+catwvid = []
 
-with io.open("CAvideos.csv", newline='', encoding='utf-8') as videos:
+numc = len(sys.argv)
+if numc < 1:
+	print("not enough parameters\n")
+	sys.exit()
+filename = sys.argv[1]
+
+with io.open(filename, newline='', encoding='utf-8') as videos:
 	csvReader = csv.reader(videos)
 	next(csvReader,None)
 	for row in csvReader:
 		var = row[category]
+		vidid = row[id]
 		if var not in categories and var is not "category_id":
 			categories.append(var)
+			catwvid.append([var,vidid])
 categories.sort()
 print(categories)
+print(catwvid)
